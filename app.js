562db8d2c933a85f9318e89cf4ee29051221896c
Yuwono Jitu Idi Pangestu <yuwonojituidipangestu@gmail.com>
2024-08-23 20:03:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ const INDEX_ROUTES = require("./routes/index");
 
 const app = express();
 
+app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(INDEX_ROUTES);
 
 
